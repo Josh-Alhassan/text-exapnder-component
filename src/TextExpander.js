a77477cedac6470006d8e56028ed4794265f15f9
Josh-Alhassan <saintjosh26@gmail.com>
2024-11-02 16:16:20 +0100

    complete project

--- src/TextExpander.js
+++ src/TextExpander.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-function TextExpander() {
-  return <div>TextExpander</div>;
+function TextExpander({
+  collaspedNumWords = 10,
+  expandButtonText = "Show more",
+  collaspeButtonText = "Show less",
+  buttonColor = "#1f09cd",
+  expanded = false,
+  className,
+  children,
+}) {
+  const [isExpanded, setIsExpanded] = useState(expanded);
+
+  const displayText = isExpanded
+    ? children
+    : children.split("").slice(0, collaspedNumWords).join("") + "...";
+
+  const buttonStyle = {
+    background: "none",
+    border: "none",
+    font: "inherit",
+    cursor: "pointer",
+    marginLeft: "6px",
+    color: buttonColor,
+  };
+
+  return (
+    <div className={className}>
+      <span>{displayText}</span>
+      <button onClick={() => setIsExpanded((exp) => !exp)} style={buttonStyle}>
+        {isExpanded ? collaspeButtonText : expandButtonText}
+      </button>
+    </div>
+  );
 }
 
 export default TextExpander;
